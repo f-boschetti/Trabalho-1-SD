623af25633d226b35bc221ae1f0154a29312fa7d
felipeboss232 <felipeboss232@gmail.com>
2019-10-01 20:59:43 -0300

    Somador 16 bits

---
+++ Somador_16bits.vhdl
@@ -0,0 +1,34 @@
+LIBRARY work;
+USE ieee.std_logic_1164.all;
+
+ENTITY somador_16bits IS
+
+port(a, b: in STD_LOGIC_VECTOR(15 DOWNTO 0);
+	 cin: in STD_LOGIC;
+	 cout: out STD_LOGIC_VECTOR(15 DOWNTO 0);
+	 resul: out STD_LOGIC_VECTOR(15 DOWNTO 0)
+	 );
+END ENTITY somador_16bits;
+
+
+ARCHITECTURE arch_somador_16bits OF somador_16bits IS
+BEGIN
+
+	process(a, b cin)
+    VARIABLE soma : std_logic_vector(15 DOWNTO 0);--NÃO SEI SE TETM QUE SER ANTES DO PROCESS
+    VARIABLE c : STD_LOGIC; --NÃO SEI SE TETM QUE SER ANTES DO PROCESS
+    BEGIN
+    	c => cin;
+    	for i in 0 to 15 loop 
+    	soma(i) := a(i) xor b(i) xor c;
+    	c := (a(i) and c(i)) or ((a(i) xor b(i)) and c);
+    	end loop;
+    
+    cout <= c;
+    resul <= somador
+    
+    end process;
+    	
+    
+	
+END ARCHITECTURE arch_somador_16bits; -- Of entity somador_16bits
