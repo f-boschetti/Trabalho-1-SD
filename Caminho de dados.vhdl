a1e4ef93992878e5c919b2bb420e95addf4f42ad
felipeboss232 <felipeboss232@gmail.com>
2019-10-08 23:37:15 -0300

    VER COM O PROFESSOR URGENTE

---
+++ Caminho de dados.vhdl
@@ -0,0 +1,28 @@
+
+--###########################
+--#   VER COM O PROFESSOR   #
+--###########################
+
+
+--Não entendi como implementar o multiplicador no projeto
+--Provavelmente vai ser preciso colocar todos os projetos em um arquivo só pois não sei como funciona
+-- o sistema para importar "component" em VHDL, talvez ele considere tudo do mesmo diretorio, porem não encontrei nada
+--na internet
+
+--mas uma possibilidade é copiar todos os codigos em um só 
+
+
+--primeiro
+	--fazer a descrição completa de todos os componentes "menores" com entity e architecture normal,
+	--exatamente igual aos projetos originais
+
+--segundo
+	-- fazer o projeto "maior" e declarar na architecture os componentes e seus port maps
+	-- (isso antes dos sinais e do begin da architecture)
+	
+--terceiro 
+	--fazer o port map dos sinais depois do begin da architecture
+	
+	
+	
+-- OUTRA OPÇÃO é tambem usar como function, porem eu nao pesquisei como usar ainda
