848aef5000d1a2e1183a141a30d949b3a90fad8f
felipeboss232 <felipeboss232@gmail.com>
2019-10-09 22:09:17 -0300

    ULA MULTIPLICADORA E SOMADORA PRONTAE COMENTADA

--- Multiplicador_16Bits.vhdl
+++ Multiplicador_16Bits.vhdl
@@ -9,16 +9,57 @@ use ieee.numeric_std.all;
 
 ENTITY multiplicador IS
   PORT(
-    a : IN           std_logic_vector (15 downto 0);
-    b : IN           std_logic_vector (15 downto 0);
-    produto : OUT    std_logic_vector (31 downto 0)
+    a :       IN     std_logic_vector (15 downto 0);
+    b :       IN     std_logic_vector (15 downto 0);
+    seletor:    IN   std_logic;
+    overflow:   out  std_logic;
+    resultado : OUT  std_logic_vector (31 downto 0)
   );
-  
-  
 END ENTITY multiplicador;
 
 ARCHITECTURE arch OF multiplicador IS
-BEGIN 
 
-produto <= a * b;
+	signal intermediario: std_logic_vector(31 downto 0)
+	
+BEGIN
+
+	PROCESS(a, b, seletor)
+	-- selecao do multiplicador
+	if (seletor = '0') then
+	
+		--faz a operacao
+ 		intermediario <= std_logic_vector(UNSIGNED a * UNSIGNED  b);
+ 			-- se os bits mais a esquerda do vetor forem maiores que zero
+ 			--overflow recebe 1 e o resoltado vai para a saida 
+ 			if (intermediario(31 downto 16) > "0000000000000000") then
+ 				overflow <= '1';
+ 				resultado <= intermediario(15 downto 0);
+ 			end if;
+ 			
+ 			--resultado vai para a saida e overflow continua em zero
+ 			else
+ 				resultado <= intermediario(15 downto 0);
+ 				overflow <= '0';
+ 			end else;
+ 	end if;
+ 	
+ 	-- seleciona adicao
+ 	if (seletor = '1') then
+ 		intermediario <= std_logic_vector(UNSIGNED a + UNSIGNED  b);
+ 		
+ 		-- se os bits mais a esquerda do vetor forem maiores que zero
+ 		--overflow recebe 1 e o resoltado vai para a saida
+ 		if (intermediario(31 downto 16) > "0000000000000000") then
+ 			overflow <= '1';
+ 			resultado <= intermediario(15 downto 0);
+ 		end if;
+ 		
+ 		--resultado vai para a saida e overflow continua em zero
+ 		else
+ 				resultado <= intermediario(15 downto 0);
+ 				overflow <= '0';
+ 			end else;
+ 	end if;
+ 		
 END ARCHITECTURE arch;
+    
